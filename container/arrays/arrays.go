a4332efea378067883a669b831018ffeafaa1a11
xiaoguoping <xiaoguoping@meituan.com>
2018-09-06 10:10:14 +0800

    fixed

--- container/arrays/arrays.go
+++ container/arrays/arrays.go
@@ -10,10 +10,10 @@ func printArray(arr [5]int) {
 }
 
 func main() {
-	var arr1 [5]int
-	arr2 := [3]int{1, 3, 5}
-	arr3 := [...]int{2, 4, 6, 8, 10}
-	var grid [4][5]int
+	var arr1 [5]int // 只赋值，不初始化
+	arr2 := [3]int{1, 3, 5} // :=的形式，要给定个数和初始值
+	arr3 := [...]int{2, 4, 6, 8, 10} // :=的形式，[...]可以让系统给你数个数
+	var grid [4][5]int // 二维数组
 
 	fmt.Println("array definitions:")
 	fmt.Println(arr1, arr2, arr3)
